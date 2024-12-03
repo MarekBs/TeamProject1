e23c9dad6d70b20d62f92bdfa0289d86252478cf
Robert Dobry <robert.dobry98@gmail.com>
2024-12-03 23:39:11 +0100

    zapisnica 5

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function toggleCollapse(collapseId) {
-    const collapses = ['meeting-notes-collapse', 'meeting-notes-collapse2'];
+    const collapses = [  'meeting-notes-collapse',
+                         'meeting-notes-collapse2',
+                         'meeting-notes-collapse3',
+                         'meeting-notes-collapse4',
+                         'meeting-notes-collapse5'];
     collapses.forEach(id => {
         if (id !== collapseId) {
         $('#' + id).collapse('hide');
