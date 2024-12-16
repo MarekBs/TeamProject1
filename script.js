59c70744abb7542a953df2f500d23eb784ad74ae
Robert-Dobry <xdobryr@stuba.sk>
2024-12-16 22:14:35 +0100

    Update Zapisnica 6

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function toggleCollapse(collapseId) {
                          'meeting-notes-collapse2',
                          'meeting-notes-collapse3',
                          'meeting-notes-collapse4',
-                         'meeting-notes-collapse5'];
+                         'meeting-notes-collapse5',
+                         'meeting-notes-collapse6'];
     collapses.forEach(id => {
         if (id !== collapseId) {
         $('#' + id).collapse('hide');
